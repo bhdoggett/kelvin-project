9c2f327bee3d183bc1ea052680feae7c890180a7
bhdoggett <55600814+billy230@users.noreply.github.com>
2024-08-16 13:09:57 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const kelvin = 0; // forecasted temperature, mesured in kelvin
+var celsius = kelvin - 273; // converting kelvin units to celsius
+
+var farenheit = (celsius * (9 / 5) + 32); // converting celsius units to farenheit
+
+var floorFarenheit = Math.floor(farenheit); // rounding farenheit down to the nearest smaller integer
+
+console.log(`The temperature is ${floorFarenheit} degrees Farenheit`);
+
+
+var newton = (celsius * 0.33);
+var floorNewton = Math.floor(newton);
+
+console.log(`The temperature is ${floorNewton} degrees Newton`);
